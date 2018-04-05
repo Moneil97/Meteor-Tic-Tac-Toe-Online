9e5079d593971c1d6c973d9f073a3f728ecb0dde
Moneil <cam8059@yahoo.com>
2018-04-04 23:55:28 -0400

    server test code

--- TTTonline/client/main.js
+++ TTTonline/client/main.js
@@ -5,7 +5,7 @@ import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
-  this.counter = new ReactiveVar(0);
+  this.counter = new ReactiveVar("bob");
 });
 
 Template.hello.helpers({
@@ -17,6 +17,8 @@ Template.hello.helpers({
 Template.hello.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+    instance.counter.set(Game.findOne().val);
+    Game.remove({_id: Game.findOne()._id});
+    Meteor.call("addJim");
   },
 });
--- TTTonline/server/main.js
+++ TTTonline/server/main.js
@@ -3,3 +3,12 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+Meteor.methods({
+  "addJim"() {
+    Game.insert({
+      val: "jim"
+    });
+    console.log("added Jim");
+  },
+});
